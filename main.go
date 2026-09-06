81eaee69b1d24b79348379a0cf59a9da0b32a9a7
VextorHorizon <coptersrichanth@gmail.com>
2026-09-06 23:55:09 +0700

    finished: v0.0.1 Prototype ground

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"net" //net.Dial
+	"strconv"
 )
 
 func main() {
 
-	IPAddress := "127.0.0.1"
+	ip := "127.0.0.1"
+	port := 5354
 
-	conn, err := net.Dial("tcp", IPAddress)
+	address := ip + ":" + strconv.Itoa(port)
+
+	// Itoa = Integer to ASCII
+
+	// fmt.Println(address)
+
+	conn, err := net.Dial("tcp", address) // connecting a connection to the port from address(ip)
 
 	if err != nil {
-		fmt.Println("Error from conn")
+		fmt.Printf("Port %d is closed!", port)
+	}
+
+	// fmt.Println(conn)
+
+	if conn != nil {
+		fmt.Printf("Port %d is open!", port)
 	}
 }
